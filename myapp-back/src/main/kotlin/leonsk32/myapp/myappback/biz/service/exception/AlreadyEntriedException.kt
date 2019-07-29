5bb10b1c56b7bfa0858369155f31e8845501fa48
leon <leonsk32@gmail.com>
2019-07-29 19:29:43 +0900

    AlreadyEntriedException を BadRequestException の子クラス化

--- myapp-back/src/main/kotlin/leonsk32/myapp/myappback/biz/service/exception/AlreadyEntriedException.kt
+++ myapp-back/src/main/kotlin/leonsk32/myapp/myappback/biz/service/exception/AlreadyEntriedException.kt
@@ -1,5 +1,5 @@
 package leonsk32.myapp.myappback.biz.service.exception
 
-class AlreadyEntriedException(message: String): Exception(message) {
+class AlreadyEntriedException(message: String): BadRequestException(message) {
 
 }
---
+++ myapp-back/src/main/kotlin/leonsk32/myapp/myappback/biz/service/exception/BadRequestException.kt
@@ -0,0 +1,7 @@
+package leonsk32.myapp.myappback.biz.service.exception
+
+import java.lang.Exception
+
+open class BadRequestException(message: String): Exception(message) {
+
+}
