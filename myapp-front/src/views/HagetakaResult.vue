6c211daeba751a4172aed7a6b4bee5866641c4cd
leon <leonsk32@gmail.com>
2019-07-31 23:44:22 +0900

    ソート機能

--- myapp-front/src/views/HagetakaResult.vue
+++ myapp-front/src/views/HagetakaResult.vue
@@ -5,27 +5,28 @@
 </template>
 
 <script>
-
 export default {
   data() {
     return {
-        lists: []
+      lists: []
     }
   },
-  created: function(){
-    var vue = this;
+  created: function() {
+    var vue = this
 
-    firebase.firestore().collection('hagetaka').onSnapshot(function(snapshot) {
+    firebase
+      .firestore()
+      .collection("hagetaka")
+      .onSnapshot(function(snapshot) {
         snapshot.docChanges().forEach(function(change) {
-            if (change.type === 'added') {
-                vue.lists.push(change.doc.data())
-            }
-            // if (change.type === 'removed') {
-            //     console.log(`removed doc ${change.doc}`)
-            // }
+          if (change.type === "added") {
+            vue.lists.push(change.doc.data())
+            vue.lists.sort((a, b) => b.value - a.value)
+          }
         })
-    })
-  }
-}
-
+      })
+  
+  },
+  computed: {}
+};
 </script>
