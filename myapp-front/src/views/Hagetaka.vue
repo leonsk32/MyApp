ead45c4531a4265145ef306ee615d84c517ea885
leon <leonsk32@gmail.com>
2019-08-01 23:17:50 +0900

    フォームのバリデーション

--- myapp-front/src/views/Hagetaka.vue
+++ myapp-front/src/views/Hagetaka.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="bootstrap-sample">
     <b-form>
-      <b-form-group id="sample-group" label="入力してね" label-for="sample-text">
-        <b-form-input placeholder="name" v-model="name"></b-form-input>
-        <b-form-input placeholder="value" v-model="value"></b-form-input>
+      <b-form-group label="入力してね">
+        <b-form-input placeholder="name" v-model="name" :state="isValidName"></b-form-input>
+        <b-form-input placeholder="0-999" v-model="value" :state="isValidValue"></b-form-input>
       </b-form-group>
       <b-button v-on:click="doAdd" variant="primary">送る</b-button>
     </b-form>
@@ -12,24 +12,46 @@
 
 <script>
 
-export default {
-  data() {
-    return {
+  export default {
+    data() {
+      return {
         name: "",
         value: ""
-    }
-  },
+      }
+    },
 
-  methods: {
-    doAdd(){
+    methods: {
+      doAdd() {
         const now = new Date();
         firebase.firestore().collection('hagetaka').add({
-            name: this.name,
-            value: this.value,
-            date: now
+          name: this.name,
+          value: this.value,
+          date: now
         })
+      }
+    },
+
+    computed: {
+      isValidName() {
+        if (this.name === "") {
+          return null
+        }
+
+        const pattern = /^[0-9a-zA-Z]*$/g
+        return pattern.test(this.name)
+      },
+      isValidValue() {
+        if (this.value === "") {
+          return null
+        }
+
+        const pattern = /^\d*$/g
+        if (!pattern.test(this.value)) return false;
+
+        const number = Number(this.value);
+        return number >= 0 && number <= 999
+      }
     }
   }
-}
 
 </script>
